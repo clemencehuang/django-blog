02ff0b1477720932bae5cac50013f6fc90e970fe
clemence huang <clemencehuang@gmail.com>
2024-06-24 10:27:59 +0000

    created POST model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+# Note: In publishing, a slug is a short name for an article that is still in production. 
+# It comes from the lead casts used in print typesetting. You can tell Django was created 
+# for the newspaper industry! In Django, the slug is what you'll use to build a URL for each of your posts. 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
