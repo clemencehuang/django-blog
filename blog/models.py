d0c2f401a5f9d67d2b27a64a6a9f2cb672c8d804
clemence huang <clemencehuang@gmail.com>
2024-06-25 12:18:50 +0000

    models part 2

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,11 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+class Meta:
+    ordering = ["-created_on", "author"]
+
+def __str__(self):
+    return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -31,5 +36,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+class Meta:
+    ordering = ["created_on"]
+
+def __str__(self):
+    return f"Comment {self.body} by {self.author}"
 
 
